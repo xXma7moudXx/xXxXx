9b3c3b004ed44db2752823266a5f465b140d8ab7
xXma7moudXx <65374951+xXma7moudXx@users.noreply.github.com>
2020-05-15 02:47:21 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,11 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on("ready", () => {
+let channel =     client.channels.get("710241094876659742")
+setInterval(function() {
+channel.send(`3mk ma7moud yad`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
